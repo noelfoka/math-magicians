6b0f7a55e404a589a717448b8387bf70803a51d4
noelfoka <nomgnenoel@gmail.com>
2023-08-09 17:27:09 +0100

    update eslint

--- src/App.js
+++ src/App.js
@@ -1,17 +1,13 @@
-import { Component } from 'react';
 import Calculator from './components/Calculator';
 import Quote from './components/Quotes';
 
-// eslint-disable-next-line react/prefer-stateless-function
-class App extends Component {
-  render() {
-    return (
-      <>
-        <Quote />
-        <Calculator />
-      </>
-    );
-  }
+function App() {
+  return (
+    <>
+      <Quote />
+      <Calculator />
+    </>
+  );
 }
 
 export default App;
