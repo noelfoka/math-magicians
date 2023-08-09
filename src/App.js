78c52a6bcf8a91e5693c6e2a06530f7dbefd6762
noelfoka <nomgnenoel@gmail.com>
2023-08-09 15:41:06 +0100

    implement api

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { Component } from 'react';
 import Calculator from './components/Calculator';
+import Quote from './quote/Quotes';
 
 // eslint-disable-next-line react/prefer-stateless-function
 class App extends Component {
   render() {
     return (
       <>
+        <Quote />
         <Calculator />
       </>
     );
