7b3e3eb73f25a2d4d26d379dad6e29d7132bb8d9
noelfoka <nomgnenoel@gmail.com>
2023-08-17 07:39:31 +0100

    fix eslint

--- src/routes-component/Home.js
+++ src/routes-component/Home.js
@@ -1,5 +1,5 @@
-import React from "react";
-import styles from "./Home.module.css";
+import React from 'react';
+import styles from './Home.module.css';
 
 function Home() {
   return (
@@ -15,10 +15,10 @@ function Home() {
         they were very large and expensive, and were not suitable for consumer
         use. This changed in the 1960s when portable electronic calculators
         began to be marketed. The first consumer portable electronic calculator
-        was the "Cal Tech", launched in 1967 by Texas Instruments. It was
-        capable of performing all four basic operations, but cost over $200,
-        making it a luxury item at the time. Over the years, calculators have
-        become more and more sophisticated and affordable. Scientific
+        was the &rdquo;Cal Tech&rdquo;, launched in 1967 by Texas Instruments.
+        It was capable of performing all four basic operations, but cost over
+        $200, making it a luxury item at the time. Over the years, calculators
+        have become more and more sophisticated and affordable. Scientific
         calculators appeared in the 1970s and were followed by graphing
         calculators in the 1980s. Nowadays, calculators are equipped with many
         advanced features such as graphing, statistics, and even programming. In
